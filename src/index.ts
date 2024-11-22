21a309219d89f6492da73354ce084a07a410a02c
akhmadshin <qwasderfasder@gmail.com>
2024-11-23 02:00:24 +0700

    Minor fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-'use client'
-
 export { handleBeforePopState } from './handle-before-pop-state';
 export { startTransition } from './start-transition';
 export { WithTransitions } from './WithTransitions';
